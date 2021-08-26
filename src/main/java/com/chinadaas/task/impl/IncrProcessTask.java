fa5491be6a398170ac2354a87d9fae93730655a3
liubc <liubaochen@chinadaas>
2021-08-26 13:05:39 +0800

    test

--- src/main/java/com/chinadaas/task/impl/IncrProcessTask.java
+++ src/main/java/com/chinadaas/task/impl/IncrProcessTask.java
@@ -1,10 +1,12 @@
 package com.chinadaas.task.impl;
 
+import com.chinadaas.common.util.EntIdListOptHandler;
 import com.chinadaas.task.IncrTask;
 import com.chinadaas.task.subtask.MembersProcessTask;
 import com.chinadaas.task.subtask.SuperCorporationProcessTask;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 /**
@@ -14,16 +16,20 @@ import org.springframework.stereotype.Component;
  * @createTime 2021.08.16
  */
 @Slf4j
+@Order(2)
 @Component
 public class IncrProcessTask implements IncrTask {
 
+    private final EntIdListOptHandler entIdListOptHandler;
     private final SuperCorporationProcessTask superCorporationProcessTask;
     private final MembersProcessTask membersProcessTask;
 
     @Autowired
-    public IncrProcessTask(SuperCorporationProcessTask superCorporationProcessTask,
+    public IncrProcessTask(EntIdListOptHandler entIdListOptHandler,
+                           SuperCorporationProcessTask superCorporationProcessTask,
                            MembersProcessTask membersProcessTask) {
 
+        this.entIdListOptHandler = entIdListOptHandler;
         this.superCorporationProcessTask = superCorporationProcessTask;
         this.membersProcessTask = membersProcessTask;
     }
@@ -39,6 +45,7 @@ public class IncrProcessTask implements IncrTask {
     }
 
     private void generateMembers() {
+        this.entIdListOptHandler.replacePreWithMembersEntIds();
         this.membersProcessTask.generateMembers();
     }
 }
